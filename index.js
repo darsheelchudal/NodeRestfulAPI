bb7d2ac1040256307c160161dec3a5732ef5470a
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-02 21:09:32 +0545

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8000;
+const MONGO_URI =
+  "mongodb+srv://darsheelchudal11:darsheel@cluster0.i4rjtg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 //routes
 app.get("/", (req, res) => {
   res.send("You are at homepage");
 });
 
-app.listen(8000, () => {
-  console.log(`Node API app is running on PORT ${8000}`);
+app.get("/blog", (req, res) => {
+  res.send("Hello blog");
 });
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongodb");
+    app.listen(8000, () => {
+      console.log(`Node API app is running on PORT ${8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error : ", error);
+  });
