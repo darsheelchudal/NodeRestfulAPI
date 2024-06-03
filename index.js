a917e461bc41b0938ccaa3fa792948a5d67ca356
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-03 08:05:58 +0545

    final commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const MONGO_URI =
 
 //middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.get("/", (req, res) => {
@@ -19,6 +20,25 @@ app.get("/blog", (req, res) => {
   res.send("Hello blog");
 });
 
+app.get("/product/", async (req, res) => {
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/product", async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -26,6 +46,39 @@ app.post("/product", async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ error: error.message });
+  }
+});
+
+//Update aproduct
+app.put("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    //cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with id : ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any product with id : ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json();
   }
 });
 
