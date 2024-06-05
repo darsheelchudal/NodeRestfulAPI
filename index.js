152c4be6723fcb7be7ee69264a62ecad31ba52e8
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-06-05 06:39:01 +0545

    product Route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { Product } from "./models/productModel.js";
 import dotenv from "dotenv";
+import productRoute from "./routes/productRoute.js";
 
 dotenv.config();
 
@@ -14,74 +15,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
+app.use("/api/product", productRoute);
+
+//routes
 app.get("/", (req, res) => {
   res.send("You are at homepage");
 });
 
 app.get("/blog", (req, res) => {
   res.send("Hello blog");
-});
-
-app.get("/product/", async (req, res) => {
-  try {
-    const product = await Product.find({});
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/product", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ error: error.message });
-  }
-});
-
-//Update aproduct
-app.put("/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    //cannot find any product in database
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with id : ${id}` });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete("/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `Cannot find any product with id : ${id}` });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json();
-  }
 });
 
 mongoose
---
+++ routes/productRoute.js
@@ -0,0 +1,68 @@
+import express from "express";
+import { Product } from "../models/productModel.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//Update aproduct
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    //cannot find any product in database
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with id : ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find any product with id : ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json();
+  }
+});
+
+export default router;
